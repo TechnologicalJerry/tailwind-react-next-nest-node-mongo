d78330aac037add7698df2252066915740f3f1ef
Rajat Verma <cserajatverma@gmail.com>
2026-01-13 12:29:18 +0530

    Add config, Mongoose, and feature modules to AppModule
    Integrated ConfigModule for environment configuration and MongooseModule for MongoDB connection. Added UsersModule, ProductsModule, and AuthModule to the AppModule imports for modular structure.

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI ||
+        'mongodb://localhost:27017/tailwind-angular-nest-node-mongo',
+    ),
+    UsersModule,
+    ProductsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
